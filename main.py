4b80a11ec4d3907fab0bfe0581f7c8c9b79d35e3
PhaseQubit <161123318+PhaseQubit@users.noreply.github.com>
2024-10-09 21:14:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from data_preprocessing import load_and_preprocess_data
 from modeling import train_and_evaluate_model
-from visualization import plot_scatter_with_regression
+from visualization import plot_scatter_with_regression, plot_comparison
 
 
 def main():
-    data = load_and_preprocess_data('AmesHousing.csv')#preprocessing function
-    print(data.head())#Print first few rows of data to understand the data structure
+    data = load_and_preprocess_data('AmesHousing.csv')
+    print(data.head())
+    print("Columns in dataset:", data.columns)
 
-    print("Columns in dataset:", data.columns)#Print all the columns
-    plot_scatter_with_regression(data, 'Gr Liv Area', 'SalePrice')#Plot Gr Liv Area vs SalePrice
+    plot_scatter_with_regression(data, 'Gr Liv Area', 'SalePrice')
 
-    mse, rmse, r2 = train_and_evaluate_model(data)#Train and evaluate the model
-    print(f'Mean Squared Error: {mse}')
-    print(f'R-squared: {r2}')
-    print(f'Root mean Squared Error: {rmse}')
+    mse_lr, rmse_lr, r2_lr, mse_rf, rmse_rf, r2_rf = train_and_evaluate_model(data)
+
+    print(f'Linear Regression - MSE: {mse_lr}, RMSE: {rmse_lr}, R-squared: {r2_lr}')
+    print(f'Random Forest - MSE: {mse_rf}, RMSE: {rmse_rf}, R-squared: {r2_rf}')
+
+    plot_comparison(mse_lr, rmse_lr, mse_rf, rmse_rf)
 
 
 if __name__ == "__main__":
